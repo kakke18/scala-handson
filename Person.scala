acc615b5e55f4e9a669bdc407d01bea2ba014dd0
kakke18 <tanaka-kakeru-xm@ynu.jp>
2019-01-08 10:54:06 +0900

    fix: Syntax error

--- Person.scala
+++ Person.scala
@@ -1,8 +1,8 @@
-class Person(name: String, age: Int, private weight: Int)
+class Person(name: String, age: Int, private val weight: Int)
 
 // オブジェクト名がPerson以外だとエラー
 // 以下のようなものをコンパニオンオブジェクトという
-Object Person {
+object Person {
   def printweight() : Unit = {
     val taro = new Person("Taro", 20, 70)
     println(taro.weight)
